56fa00be50d3701050e5c46698b5ad25eac0fa25
biswajeetbehera123 <107670354+biswajeetbehera123@users.noreply.github.com>
2023-10-25 11:44:26 +0530

    bug fixing

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Login from './components/Login';
-import MainContainer from './components/MainContainer';
-import Welcome from './components/Welcome';
-import ChatArea from './components/ChatArea';
-import Users from './components/Users';
-import CreateGroups from './components/CreateGroups';
-import Groups from "./components/Groups"
+import React from "react";
+import "./App.css";
+import MainContainer from "./Components/MainContainer";
+import Login from "./Components/Login";
+import { Route, Routes } from "react-router-dom";
+import Welcome from "./Components/Welcome";
+import ChatArea from "./Components/ChatArea";
+import Users from "./Components/Users";
+import CreateGroups from "./Components/CreateGroups";
+import Groups from "./Components/Groups";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
+  const dispatch = useDispatch();
+  const lightTheme = useSelector((state) => state.themeKey);
   return (
-    <div className="App">
+    <div className={"App" + (lightTheme ? "" : "-dark")}>
+      {/* <MainContainer /> */}
+      {/* <Login /> */}
       <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='app' element={<MainContainer />}>
-          <Route path='welcome' element={<Welcome />} ></Route>
-          <Route path='chat' element={<ChatArea />} ></Route>
-          <Route path='users' element={<Users />} ></Route>
-          <Route path='groups' element={<Groups />} ></Route>
-          <Route path='create-groups' element={<CreateGroups />} ></Route>
+        <Route path="/" element={<Login />} />
+        <Route path="app" element={<MainContainer />}>
+          <Route path="welcome" element={<Welcome />}></Route>
+          <Route path="chat/:_id" element={<ChatArea />}></Route>
+          <Route path="users" element={<Users />}></Route>
+          <Route path="groups" element={<Groups />}></Route>
+          <Route path="create-groups" element={<CreateGroups />}></Route>
         </Route>
       </Routes>
     </div>
